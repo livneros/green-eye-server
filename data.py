78b1996355f0ccada4cb6ebe31d0e448bbcaed7a
livneros <livneros@gmail.com>
2019-01-27 16:34:07 +0200

    running kmeans on train set only

--- data.py
+++ data.py
@@ -9,11 +9,11 @@ def reshape(image):
 
 
 train_images = train_images.reshape(reshape(train_images))
-# images = train_images
+images = train_images
 train_labels = mnist.train_labels()
 test_images = mnist.test_images()
 test_images = test_images.reshape(reshape(test_images))
 test_labels = mnist.test_labels()
-images = np.concatenate((train_images, test_images))
+# images = np.concatenate((train_images, test_images))
 # images = np.array([np.array([1, 2]), np.array([10, 20]), np.array([100, 200]),
 #                    np.array([2, 3]), np.array([11, 21]), np.array([101, 201])])
