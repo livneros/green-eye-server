bab8be842917a372292dbd4cb8823825d9dec578
livneros <livneros@gmail.com>
2019-01-28 09:08:07 +0200

    getElementsByClassName and not by id

--- static/homepage.js
+++ static/homepage.js
@@ -18,7 +18,7 @@ function loadImages() {
     xhttp.onload = function()
         {
             if (this.status === 200){
-                document.getElementById("images").innerHTML = this.responseText;
+                document.getElementsByClassName("images").innerHTML = this.responseText;
             }
         }
     xhttp.send();
