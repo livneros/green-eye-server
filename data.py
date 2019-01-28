54ee6e3b57cc7d1bcb1cc3c7ee01ad3c5037c2be
livneros <livneros@gmail.com>
2019-01-28 11:08:02 +0200

    changing data source

--- data.py
+++ data.py
@@ -1,17 +1,18 @@
 import mnist
 import numpy as np
 
-train_images = mnist.train_images()
+# train_images = mnist.train_images()
 
 
 def reshape_image_to_1D(image):
     return (image.shape[0], image.shape[1] * image.shape[1])
 
 
-train_images = train_images.reshape(reshape_image_to_1D(train_images))
-images = train_images[:20000]
-train_labels = mnist.train_labels()
+# train_images = train_images.reshape(reshape_image_to_1D(train_images))
+# images = train_images[:20000]
+# train_labels = mnist.train_labels()
 test_images = mnist.test_images()
-test_images = test_images.reshape(reshape_image_to_1D(test_images))
-test_labels = mnist.test_labels()
+images = test_images
+# test_images = test_images.reshape(reshape_image_to_1D(test_images))
+# test_labels = mnist.test_labels()
 # images = np.concatenate((train_images, test_images))
