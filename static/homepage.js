3596c5a130f884f4e9db22709dbf0d075474789b
livneros <livneros@gmail.com>
2019-01-28 12:29:08 +0200

    consoles

--- static/homepage.js
+++ static/homepage.js
@@ -12,7 +12,7 @@ function valid_n_clusters(n_clusters){
 }
 
 function loadImages() {
-    print(document.getElementById("loader").style.display)
+    console.log(document.getElementById("loader").style.display)
     document.getElementById("images").innerHTML = '';
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', SERVER_PATH , true)
